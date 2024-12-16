fb0782c5e027172f39f67c9ad76e521be3306113
PavithranDev <pavithran.profession@gmail.com>
2024-12-16 15:13:43 +0530

    Create TsunamiReport.c

---
+++ TsunamiReport.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int dead,inju,safe;
+    scanf("%d %d %d",&dead,&inju,&safe);
+    printf("1)dead: %d\n",dead);
+    printf("2)injured: %d\n",inju);
+    printf("3)safe: %d\n",safe);
+    printf("Please help the people who are suffering!!!");
+    return 0;
+}
