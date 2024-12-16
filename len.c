5dfd43016869987c07d5506865bb6857304f1902
PavithranDev <pavithran.profession@gmail.com>
2024-12-16 15:12:38 +0530

    Create len.c

---
+++ len.c
@@ -0,0 +1,11 @@
+#include<stdio.h> 
+#include<string.h> 
+int main() 
+{ 
+    char a[100]; 
+    int l;
+    scanf("%s",a); 
+    l=strlen(a);
+    printf("\nThe length of %s is %d",a,l); 
+    return 0; 
+}  
