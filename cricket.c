e4894ae213eea4593e8cab71431e71b22fe25d49
PavithranDev <pavithran.profession@gmail.com>
2024-12-16 15:15:13 +0530

    Create cricket.c

---
+++ cricket.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+   int length,quantity;
+   scanf("%d %d",&length,&quantity);
+   printf("%d\n",2*(length+quantity));
+   printf("%d",length*quantity);
+    
+    return 0;
+}
